38243d2c0af9d23137707e68575ece1a6234c4a1
Ji Park <ji.park@jpnws.com>
2024-03-01 00:49:01 -0800

    Add CRUD operations for posts using Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,93 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
+import { PrismaClient } from '@prisma/client';
 
-export const createApp = () => {
+type Postbody = {
+  id: string;
+  subject: string;
+  text: string;
+  boardId: number;
+  userId: number;
+};
+
+export const createApp = (prisma: PrismaClient) => {
   const app = new Elysia();
 
-  app.get('/boards/:id/posts', () => {});
+  app.get('/boards/:id/posts', async ({ params: { id } }) => {
+    try {
+      return await prisma.post.findMany({
+        where: {
+          boardId: parseInt(id),
+        },
+      });
+    } catch (error) {
+      console.error('Failed to fetch posts:', error);
+    }
+  });
 
-  app.get('/posts/:id', () => {});
+  app.get('/posts/:id', ({ params: { id } }) => {
+    try {
+      return prisma.post.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
+    } catch (error) {
+      console.error('Failed to fetch post:', error);
+    }
+  });
 
-  app.post('/posts', () => {});
+  app.post('/posts', async ({ body }) => {
+    const { subject, text, boardId, userId } = body as Postbody;
+    try {
+      return await prisma.post.create({
+        data: {
+          subject,
+          text,
+          board: {
+            connect: {
+              id: boardId,
+            },
+          },
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      console.error('Failed to create post:', error);
+    }
+  });
 
-  app.put('/posts/:id', () => {});
+  app.put('/posts/:id', ({ body }) => {
+    const { id, subject, text } = body as Postbody;
+    try {
+      return prisma.post.update({
+        where: {
+          id: parseInt(id),
+        },
+        data: {
+          subject,
+          text,
+        },
+      });
+    } catch (error) {
+      console.error('Failed to update post:', error);
+    }
+  });
 
-  app.delete('/posts/:id', () => {});
+  app.delete('/posts/:id', ({ params: { id } }) => {
+    try {
+      return prisma.post.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+    } catch (error) {
+      console.error('Failed to delete post:', error);
+    }
+  });
 
   return app;
 };
