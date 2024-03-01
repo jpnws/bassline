9e9b809e5875c7f732f64b6f03f9bfc544861b12
Ji Park <ji.park@jpnws.com>
2024-02-29 23:36:47 -0800

    Add routes for boards, posts, and CRUD operations

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,15 @@ import { Elysia } from 'elysia';
 export const createApp = () => {
   const app = new Elysia();
 
-  app.get('/', () => 'Disco Bassline!');
+  app.get('/boards/:id/posts', () => {});
+
+  app.get('/posts/:id', () => {});
+
+  app.post('/posts', () => {});
+
+  app.put('/posts/:id', () => {});
+
+  app.delete('/posts/:id', () => {});
 
   return app;
 };
