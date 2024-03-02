1d527c5003737359fe57b317c8824709f4c26f6c
Ji Park <ji.park@jpnws.com>
2024-03-02 15:31:11 -0800

    Refactor app.listen to include a console stating that the app is listening on hostname and port.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ const prisma = new PrismaClient();
 
 const app = createApp(prisma, swagger);
 
-app.listen({
-  hostname: Bun.env.APP_HOST,
-  port: Bun.env.APP_PORT,
-});
+app.listen(
+  {
+    hostname: Bun.env.APP_HOST,
+    port: Bun.env.APP_PORT,
+  },
+  (server) => {
+    console.log(`Listening on ${server.hostname}:${server.port}`);
+  }
+);
