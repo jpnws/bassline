4fda429f4da74a6dd94457d7a88e75a7fd02e7b6
Ji Park <ji.park@jpnws.com>
2024-03-01 19:27:45 -0800

    Add board and user seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,41 @@ import { PrismaClient, UserRole } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.board.create({
+  const board = await prisma.board.create({
     data: {
       name: 'discoboard',
     },
   });
 
-  await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       username: 'alice',
       role: UserRole.MEMBER,
       password: 'password123',
     },
+  });
+
+  await prisma.post.createMany({
+    data: [
+      {
+        subject: 'Hello, World! 1',
+        text: 'This is my first post!',
+        boardId: board.id,
+        userId: user.id,
+      },
+      {
+        subject: 'Hello, World! 2',
+        text: 'This is my first post!',
+        boardId: board.id,
+        userId: user.id,
+      },
+      {
+        subject: 'Hello, World! 3',
+        text: 'This is my first post!',
+        boardId: board.id,
+        userId: user.id,
+      },
+    ],
   });
 }
 
