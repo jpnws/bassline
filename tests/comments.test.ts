3f77877645c34657e4ab118f183586871db17d3d
Ji Park <ji.park@jpnws.com>
2024-03-03 09:19:42 -0800

    Add test case for updating a comment

--- tests/comments.test.ts
+++ tests/comments.test.ts
@@ -56,4 +56,58 @@ describe('Comments API', () => {
     expect(comment.postId).toBe(newComment.postId);
     expect(comment.userId).toBe(newComment.userId);
   });
+
+  it('should update a comment', async () => {
+    // * ========================
+    // * Arrange
+    // * ========================
+    // Create a board.
+    const boardCreateResponse = await helper.createBoard({
+      name: 'test-board-name2',
+    });
+    const { id: boardId } = boardCreateResponse.body;
+    // Create a user.
+    const userCreateResponse = await helper.createUser({
+      username: 'test-user-username2',
+      password: 'password',
+    });
+    const { id: userId } = userCreateResponse.body;
+    const postCreateResponse = await helper.createPost({
+      subject: 'test-post-subject2',
+      text: 'test-post-text2',
+      boardId: boardId,
+      userId: userId,
+    });
+    const { id: postId } = postCreateResponse.body;
+    const commentCreateResponse = await helper.createComment({
+      text: 'test-comment-text2',
+      postId: postId,
+      userId: userId,
+    });
+    const { id: commentId } = commentCreateResponse.body;
+    // * ========================
+    // * Act
+    // * ========================
+    const updatedComment = {
+      text: 'test-comment-text2-updated',
+      postId: postId,
+      userId: userId,
+    };
+    const commentUpdateResponse = await helper.updateComment(
+      commentId,
+      updatedComment
+    );
+    // * ========================
+    // * Assert
+    // * ========================
+    expect(commentUpdateResponse.status).toBe(200);
+    const comment = commentUpdateResponse.body;
+    expect(comment.text).toBe(updatedComment.text);
+    expect(comment.postId).toBe(updatedComment.postId);
+    expect(comment.userId).toBe(updatedComment.userId);
+  });
+
+  it('should delete a comment', async () => {});
+
+  it('should return comments by post id', async () => {});
 });
