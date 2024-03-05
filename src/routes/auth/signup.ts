7f2d48fa7212a9e2f4d708110a4bec3c5d34e191
Ji Park <ji.park@jpnws.com>
2024-03-05 15:10:16 -0800

    Refactor signup route to use `app.group()` to prevent event leaking.

--- src/routes/auth/signup.ts
+++ src/routes/auth/signup.ts
@@ -6,114 +6,120 @@ import cookie from '@elysiajs/cookie';
 export const signup = (prisma: PrismaClient) => {
   const app = new Elysia();
 
-  app
-    .guard({
+  app.group(
+    '',
+    {
       body: t.Object({
         username: t.String(),
         password: t.String(),
       }),
-    })
-    .use(
-      jwt({
-        name: 'jwt',
-        secret: Bun.env.APP_JWT_SECRET,
-      })
-    )
-    .use(cookie())
-    .post(
-      '/signup',
-      async ({ body, set, jwt, cookie: { auth }, setCookie }) => {
-        // * ================================================
-        // * Check if user is already authenticated.
-        // * ================================================
-        if (auth) {
-          set.status = 409;
-          return {
-            message: 'You are already authenticated.',
-          };
-        }
-        const { username, password } = body;
-        // * ================================================
-        // * Check if username or password is empty.
-        // * ================================================
-        if (!username || !password) {
-          set.status = 400;
-          return {
-            message: 'Username or password cannot be empty.',
-          };
-        }
-        // * ================================================
-        // * Check for existing user.
-        // * ================================================
-        try {
-          const userExists = await prisma.user.findUnique({
-            where: {
-              username,
-            },
-            select: {
-              id: true,
-            },
-          });
-          if (userExists) {
-            set.status = 400;
-            return {
-              message: 'Username already in use.',
-            };
-          }
-        } catch (error) {
-          console.error('Failed to find the user:', error);
-          set.status = 500;
-          return {
-            message: 'Server error. Unable process the request.',
-          };
-        }
-
-        try {
-          // * ================================================
-          // * Hash the password.
-          // * ================================================
-          const hash = await Bun.password.hash(password);
-          // * ================================================
-          // * Save the user to the database.
-          // * ================================================
-          const user = await prisma.user.create({
-            data: {
-              username,
-              hash,
-            },
-            select: {
-              id: true,
-              username: true,
-              role: true,
-            },
-          });
-          // * ================================================
-          // * Store JWT in a cookie.
-          // * ================================================
-          setCookie(
-            'auth',
-            await jwt.sign({
-              id: user.id,
-              username: user.username,
-              role: user.role,
-            }),
-            {
-              httpOnly: true,
-              maxAge: 60 * 60 * 24 * 7,
-              path: '/',
-              secure: Bun.env.NODE_ENV === 'production',
+    },
+    (app) => {
+      app
+        .use(
+          jwt({
+            name: 'jwt',
+            secret: Bun.env.APP_JWT_SECRET,
+          })
+        )
+        .use(cookie())
+        .post(
+          '/signup',
+          async ({ body, set, jwt, cookie: { auth }, setCookie }) => {
+            // * ================================================
+            // * Check if user is already authenticated.
+            // * ================================================
+            if (auth) {
+              set.status = 409;
+              return {
+                message: 'You are already authenticated.',
+              };
             }
-          );
-          set.status = 201;
-        } catch (error) {
-          console.log('Failed to create a new user:', error);
-          set.status = 500;
-          return {
-            message: 'Server error. Unable to create an account.',
-          };
-        }
-      }
-    );
+            const { username, password } = body;
+            // * ================================================
+            // * Check if username or password is empty.
+            // * ================================================
+            if (!username || !password) {
+              set.status = 400;
+              return {
+                message: 'Username or password cannot be empty.',
+              };
+            }
+            // * ================================================
+            // * Check for existing user.
+            // * ================================================
+            try {
+              const userExists = await prisma.user.findUnique({
+                where: {
+                  username,
+                },
+                select: {
+                  id: true,
+                },
+              });
+              if (userExists) {
+                set.status = 400;
+                return {
+                  message: 'Username already in use.',
+                };
+              }
+            } catch (error) {
+              console.error('Failed to find the user:', error);
+              set.status = 500;
+              return {
+                message: 'Server error. Unable process the request.',
+              };
+            }
+
+            try {
+              // * ================================================
+              // * Hash the password.
+              // * ================================================
+              const hash = await Bun.password.hash(password);
+              // * ================================================
+              // * Save the user to the database.
+              // * ================================================
+              const user = await prisma.user.create({
+                data: {
+                  username,
+                  hash,
+                },
+                select: {
+                  id: true,
+                  username: true,
+                  role: true,
+                },
+              });
+              // * ================================================
+              // * Store JWT in a cookie.
+              // * ================================================
+              setCookie(
+                'auth',
+                await jwt.sign({
+                  id: user.id,
+                  username: user.username,
+                  role: user.role,
+                }),
+                {
+                  httpOnly: true,
+                  maxAge: 60 * 60 * 24 * 7,
+                  path: '/',
+                  secure: Bun.env.NODE_ENV === 'production',
+                }
+              );
+              set.status = 201;
+            } catch (error) {
+              console.log('Failed to create a new user:', error);
+              set.status = 500;
+              return {
+                message: 'Server error. Unable to create an account.',
+              };
+            }
+          }
+        );
+      return app;
+    }
+  );
 
   return app;
 };
