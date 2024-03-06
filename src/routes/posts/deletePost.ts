e499014c1a348c870f766e227121175ddd054d19
Ji Park <ji.park@jpnws.com>
2024-03-06 11:20:41 -0800

    Extract post deleting route to its own file.

---
+++ src/routes/posts/deletePost.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import { Elysia, t } from 'elysia';
+
+/**
+ * Delete a post by its ID.
+ *
+ * @param prisma - The Prisma client.
+ * @returns The Elysia app.
+ */
+export const deletePost = (prisma: PrismaClient) => {
+  const app = new Elysia();
+
+  app.delete(
+    '/posts/:id',
+    async ({ params: { id }, set }) => {
+      try {
+        await prisma.post.delete({
+          where: {
+            id: parseInt(id),
+          },
+        });
+        set.status = 202;
+      } catch (error) {
+        console.error('Failed to delete post:', error);
+        set.status = 500;
+      }
+    },
+    {
+      detail: {
+        tags: ['Posts'],
+      },
+    }
+  );
+
+  return app;
+};
