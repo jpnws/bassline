a5c50c67e9e88f65ed0719e0618070c9ca41d87c
Ji Park <ji.park@jpnws.com>
2024-03-06 22:17:32 -0800

    Extract out getBoardPosts route to its own file and refactor the shape of its response data and add openApi docs.

---
+++ src/routes/boards/getBoardPosts.ts
@@ -0,0 +1,101 @@
+import { PrismaClient } from '@prisma/client';
+import { Elysia } from 'elysia';
+
+/**
+ * Boards board posts.
+ *
+ * @param prisma - The Prisma client.
+ * @returns The Elysia app.
+ */
+export const getBoardPosts = (prisma: PrismaClient) => {
+  const app = new Elysia();
+
+  app.get(
+    '/boards/:id/posts',
+    async ({ params: { id }, set }) => {
+      try {
+        const board = await prisma.board.findUnique({
+          where: {
+            id: parseInt(id),
+          },
+          select: {
+            id: true,
+            name: true,
+            posts: {
+              select: {
+                id: true,
+                subject: true,
+                createdAt: true,
+                user: {
+                  select: {
+                    username: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+        set.status = 200;
+        return {
+          data: {
+            board,
+          },
+        };
+      } catch (error) {
+        console.error('Failed to retrieve posts:', error);
+        set.status = 500;
+      }
+    },
+    {
+      detail: {
+        tags: ['Boards'],
+        // OpenAPIV3.ResponsesObject
+        responses: {
+          200: {
+            description: 'Retrieved posts associated with board',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    data: {
+                      type: 'object',
+                      properties: {
+                        board: {
+                          type: 'object',
+                          properties: {
+                            id: { type: 'number' },
+                            name: { type: 'string' },
+                            posts: {
+                              type: 'array',
+                              items: {
+                                type: 'object',
+                                properties: {
+                                  id: { type: 'number' },
+                                  subject: { type: 'string' },
+                                  createdAt: { type: 'string' },
+                                  user: {
+                                    type: 'object',
+                                    properties: {
+                                      username: { type: 'string' },
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    }
+  );
+
+  return app;
+};
