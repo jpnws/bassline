ca3c078bf51a0f115853e65ab5fc98e1264d22ca
Ji Park <ji.park@jpnws.com>
2024-03-06 19:13:50 -0800

    Add Elysia JWT and Cookie dependencies, update getPostComments route

--- src/routes/posts/getPostComments.ts
+++ src/routes/posts/getPostComments.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
+import jwt from '@elysiajs/jwt';
+import cookie from '@elysiajs/cookie';
 
 /**
  * Retrieve all comments by post ID.
@@ -10,26 +12,102 @@ import { Elysia } from 'elysia';
 export const getPostComments = (prisma: PrismaClient) => {
   const app = new Elysia();
 
-  app.get(
-    '/posts/:id/comments',
-    async ({ params: { id }, set }) => {
-      try {
-        const comments = await prisma.comment.findMany({
-          where: {
-            postId: parseInt(id),
-          },
-        });
-        set.status = 200;
-        return comments;
-      } catch (error) {
-        console.error('Failed to retrieve comments:', error);
-        set.status = 500;
-      }
-    },
+  app.group(
+    '',
     {
-      detail: {
-        tags: ['Posts'],
-      },
+      params: t.Object({
+        id: t.Numeric(),
+      }),
+    },
+    (app) => {
+      app.get(
+        '/posts/:id/comments',
+        async ({ params: { id }, set }) => {
+          try {
+            const comments = await prisma.comment.findMany({
+              where: {
+                postId: id,
+              },
+              select: {
+                id: true,
+                post: {
+                  select: {
+                    id: true,
+                  },
+                },
+                user: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+                createdAt: true,
+                updatedAt: true,
+              },
+            });
+            set.status = 200;
+            return {
+              data: {
+                comments,
+              },
+            };
+          } catch (error) {
+            console.error('Failed to retrieve comments:', error);
+            set.status = 500;
+          }
+        },
+        {
+          detail: {
+            tags: ['Posts'],
+            // OpenAPIV3.ResponsesObject
+            responses: {
+              200: {
+                description: 'The comments were successfully retrieved',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                      properties: {
+                        data: {
+                          type: 'object',
+                          properties: {
+                            comments: {
+                              type: 'array',
+                              items: {
+                                type: 'object',
+                                properties: {
+                                  id: { type: 'number' },
+                                  post: {
+                                    type: 'object',
+                                    properties: {
+                                      id: { type: 'number' },
+                                    },
+                                  },
+                                  user: {
+                                    type: 'object',
+                                    properties: {
+                                      id: { type: 'number' },
+                                      username: { type: 'string' },
+                                    },
+                                  },
+                                  createdAt: { type: 'string' },
+                                  updatedAt: { type: 'string' },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        }
+      );
+
+      return app;
     }
   );
 
