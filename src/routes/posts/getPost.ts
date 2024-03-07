b7bd3863bea59c19de1ce1f3bb9eeb03ecaecf37
Ji Park <ji.park@jpnws.com>
2024-03-07 10:35:50 -0800

    Remove authorization and refactor the response data shape.

--- src/routes/posts/getPost.ts
+++ src/routes/posts/getPost.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 import { Elysia, t } from 'elysia';
-import jwt from '@elysiajs/jwt';
-import cookie from '@elysiajs/cookie';
 
 /**
  * Retrieve a single post by its ID.
@@ -20,156 +18,92 @@ export const getPost = (prisma: PrismaClient) => {
       }),
     },
     (app) => {
-      app
-        .use(
-          jwt({
-            name: 'jwt',
-            secret: process.env.APP_JWT_SECRET,
-          })
-        )
-        .use(cookie())
-        .get(
-          '/posts/:id',
-          async ({ params: { id }, jwt, set, cookie: { auth } }) => {
-            try {
-              // * ================================================
-              // * Query the requested post.
-              // * ================================================
-              const post = await prisma.post.findUnique({
-                where: {
-                  id,
-                },
-                select: {
-                  id: true,
-                  subject: true,
-                  text: true,
-                  createdAt: true,
-                  updatedAt: true,
-                  board: {
-                    select: {
-                      id: true,
-                      name: true,
-                    },
-                  },
-                  user: {
-                    select: {
-                      id: true,
-                      username: true,
-                    },
+      app.get(
+        '/posts/:id',
+        async ({ params: { id }, set }) => {
+          try {
+            // * ================================================
+            // * Query the requested post.
+            // * ================================================
+            const post = await prisma.post.findUnique({
+              where: {
+                id,
+              },
+              select: {
+                id: true,
+                subject: true,
+                text: true,
+                createdAt: true,
+                updatedAt: true,
+                board: {
+                  select: {
+                    id: true,
+                    name: true,
                   },
                 },
-              });
-              if (!post) {
-                set.status = 404;
-                return;
-              }
-              set.status = 200;
-              if (auth) {
-                // * ================================================
-                // * Verify the user's JWT.
-                // * ================================================
-                const user = (await jwt.verify(auth)) as UserBody;
-                if (!user) {
-                  // * ================================================
-                  // * Respond with ordinary payload when unauthorized.
-                  // * ================================================
-                  return {
-                    data: {
-                      post: {
-                        ...post,
-                        user: {
-                          ...post.user,
-                          isAuthor: false,
-                          isAdmin: false,
-                        },
-                      },
-                    },
-                  };
-                }
-                // * ================================================
-                // * Respond with payload for registered member.
-                // * ================================================
-                return {
-                  data: {
-                    post: {
-                      ...post,
-                      user: {
-                        ...post.user,
-                        isAuthor: user.id === post.user.id,
-                        isAdmin: user.role === 'ADMIN',
-                      },
-                    },
-                  },
-                };
-              }
-              // * ================================================
-              // * Respond with ordinary payload for anon user.
-              // * ================================================
-              return {
-                data: {
-                  post: {
-                    ...post,
-                    user: {
-                      ...post.user,
-                      isAuthor: false,
-                      isAdmin: false,
-                    },
+                author: {
+                  select: {
+                    id: true,
+                    username: true,
                   },
                 },
-              };
-            } catch (error) {
-              console.error('Failed to retrieve post:', error);
-              set.status = 500;
-              return {
-                status: 'error',
-                message: 'Failed to retrieve post',
-              };
+              },
+            });
+            if (!post) {
+              set.status = 404;
+              return;
             }
-          },
-          {
-            detail: {
-              tags: ['Posts'],
-              // OpenAPIV3.ResponsesObject
-              responses: {
-                200: {
-                  description: 'Post retrieved',
-                  content: {
-                    'application/json': {
-                      schema: {
-                        type: 'object',
-                        properties: {
-                          data: {
-                            type: 'object',
-                            properties: {
-                              post: {
-                                type: 'object',
-                                properties: {
-                                  id: { type: 'number' },
-                                  subject: { type: 'string' },
-                                  text: { type: 'string' },
-                                  createdAt: { type: 'string' },
-                                  updatedAt: { type: 'string' },
-                                  board: {
-                                    type: 'object',
-                                    properties: {
-                                      id: { type: 'number' },
-                                      name: { type: 'string' },
-                                    },
-                                  },
-                                  author: {
-                                    type: 'object',
-                                    properties: {
-                                      id: { type: 'number' },
-                                      username: { type: 'string' },
-                                    },
+            set.status = 200;
+            return {
+              data: {
+                post,
+              },
+            };
+          } catch (error) {
+            console.error('Failed to retrieve post:', error);
+            set.status = 500;
+            return {
+              status: 'error',
+              message: 'Failed to retrieve post',
+            };
+          }
+        },
+        {
+          detail: {
+            tags: ['Posts'],
+            // OpenAPIV3.ResponsesObject
+            responses: {
+              200: {
+                description: 'Post retrieved',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                      properties: {
+                        data: {
+                          type: 'object',
+                          properties: {
+                            post: {
+                              type: 'object',
+                              properties: {
+                                id: { type: 'number' },
+                                subject: { type: 'string' },
+                                text: { type: 'string' },
+                                createdAt: { type: 'string' },
+                                updatedAt: { type: 'string' },
+                                board: {
+                                  type: 'object',
+                                  properties: {
+                                    id: { type: 'number' },
+                                    name: { type: 'string' },
                                   },
                                 },
-                              },
-                              currentUser: {
-                                type: 'object',
-                                properties: {
-                                  id: { type: 'number' },
-                                  username: { type: 'string' },
+                                author: {
+                                  type: 'object',
+                                  properties: {
+                                    id: { type: 'number' },
+                                    username: { type: 'string' },
+                                  },
                                 },
                               },
                             },
@@ -179,16 +113,17 @@ export const getPost = (prisma: PrismaClient) => {
                     },
                   },
                 },
-                404: {
-                  description: 'Post not found',
-                },
-                500: {
-                  description: 'An unexpected error occurred',
-                },
+              },
+              404: {
+                description: 'Post not found',
+              },
+              500: {
+                description: 'An unexpected error occurred',
               },
             },
-          }
-        );
+          },
+        }
+      );
 
       return app;
     }
