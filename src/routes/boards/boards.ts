f7ee34ee7bbfa1bdf7fb192c3d80644bff93a4f5
Ji Park <ji.park@jpnws.com>
2024-03-06 22:16:27 -0800

    Extract out all the routes to their own files.

--- src/routes/boards/boards.ts
+++ src/routes/boards/boards.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 import { Elysia } from 'elysia';
 
+import { createBoard } from 'src/routes/boards/createBoard';
+import { getBoardPosts } from 'src/routes/boards/getBoardPosts';
+import { getBoards } from 'src/routes/boards/getBoards';
+
 /**
  * Boards route.
  * @param prisma - The Prisma client.
@@ -9,80 +13,9 @@ import { Elysia } from 'elysia';
 export const boards = (prisma: PrismaClient) => {
   const app = new Elysia();
 
-  /**
-   * Create a new board.
-   */
-  app.post(
-    '/boards',
-    async ({ body, set }) => {
-      const { name } = body as { name: string };
-      try {
-        const board = await prisma.board.create({
-          data: {
-            name,
-          },
-        });
-        set.status = 201;
-        return board;
-      } catch (error) {
-        console.error('Failed to create board:', error);
-        set.status = 500;
-      }
-    },
-    {
-      detail: {
-        tags: ['Boards'],
-      },
-    }
-  );
-
-  /**
-   * Retrieve all boards.
-   */
-  app.get(
-    '/boards',
-    async ({ set }) => {
-      try {
-        const boards = await prisma.board.findMany();
-        set.status = 200;
-        return boards;
-      } catch (error) {
-        console.error('Failed to retrieve boards:', error);
-        set.status = 500;
-      }
-    },
-    {
-      detail: {
-        tags: ['Boards'],
-      },
-    }
-  );
-
-  /**
-   * Retrieve all posts corresponding to a specific board ID.
-   */
-  app.get(
-    '/boards/:id/posts',
-    async ({ params: { id }, set }) => {
-      try {
-        const posts = await prisma.post.findMany({
-          where: {
-            boardId: parseInt(id),
-          },
-        });
-        set.status = 200;
-        return posts;
-      } catch (error) {
-        console.error('Failed to retrieve posts:', error);
-        set.status = 500;
-      }
-    },
-    {
-      detail: {
-        tags: ['Boards'],
-      },
-    }
-  );
+  app.use(createBoard(prisma));
+  app.use(getBoards(prisma));
+  app.use(getBoardPosts(prisma));
 
   return app;
 };
