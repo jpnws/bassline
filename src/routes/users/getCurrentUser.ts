1413bc4a9c19d4f6ba9d26c55d6f17edf7d72198
Ji Park <ji.park@jpnws.com>
2024-03-11 22:24:43 -0700

    Extract openApi spec out to its own variable and use dependency injection to inject userRepository into UserController constructor and use userController.getCurrentUser to handle the request.

--- src/routes/users/getCurrentUser.ts
+++ src/routes/users/getCurrentUser.ts
@@ -1,8 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { OpenAPIV3 } from 'openapi-types';
 import { Elysia } from 'elysia';
-
 import jwt from '@elysiajs/jwt';
 import bearer from '@elysiajs/bearer';
+
+import { PrismaClient } from '@prisma/client';
+
+import UserController from 'src/routes/users/UserController';
+import UserRepository from 'src/routes/users/UserRepository';
 
 /**
  * Get the current logged in user.
@@ -13,6 +17,9 @@ import bearer from '@elysiajs/bearer';
 export const getCurrentUser = (prisma: PrismaClient) => {
   const app = new Elysia();
 
+  const userRepository = new UserRepository(prisma);
+  const userController = new UserController(userRepository);
+
   app.group('', {}, (app) => {
     app
       .use(
@@ -22,120 +29,62 @@ export const getCurrentUser = (prisma: PrismaClient) => {
         })
       )
       .use(bearer())
-      .get(
-        '/users/current',
-        async ({ jwt, set, bearer }) => {
-          // * ================================================
-          // * Ensure that the user is already authenticated.
-          // * ================================================
-          if (!bearer) {
-            set.status = 400;
-            return;
-          }
-          // * ================================================
-          // * Verify the user's JWT.
-          // * ================================================
-          const authUser = (await jwt.verify(bearer)) as UserBody;
-          if (!authUser) {
-            set.status = 401;
-            return;
-          }
-          // * ================================================
-          // * Ensure that the user's JWT has valid data.
-          // * ================================================
-          if (
-            authUser.id === undefined ||
-            authUser.username === undefined ||
-            authUser.role === undefined
-          ) {
-            set.status = 401;
-            return;
-          }
-          // * ================================================
-          // * Check user existence and respond with user data.
-          // * ================================================
-          try {
-            const user = await prisma.user.findUnique({
-              where: {
-                id: authUser.id,
-                username: authUser.username,
-                role: authUser.role,
-              },
-              select: {
-                id: true,
-                username: true,
-                role: true,
-              },
-            });
-            if (!user) {
-              set.status = 404;
-              return { message: 'User not found' };
-            }
-            return {
-              data: {
-                user,
-              },
-            };
-          } catch (error) {
-            console.error('Failed to retrieve user:', error);
-            set.status = 500;
-          }
-        },
-        {
-          detail: {
-            tags: ['Users'],
-            // OpenAPIV3.ResponsesObject
-            responses: {
-              200: {
-                description: 'User retrieved',
-                content: {
-                  'application/json': {
-                    schema: {
+      .get('/users/current', userController.getCurrentUser, openApiSpec);
+
+    return app;
+  });
+
+  return app;
+};
+
+const openApiSpec = {
+  detail: {
+    tags: ['Users'],
+    // OpenAPIV3.ResponsesObject
+    responses: {
+      200: {
+        description: 'User retrieved',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                data: {
+                  type: 'object',
+                  properties: {
+                    user: {
                       type: 'object',
                       properties: {
-                        data: {
-                          type: 'object',
-                          properties: {
-                            user: {
-                              type: 'object',
-                              properties: {
-                                id: {
-                                  type: 'number',
-                                },
-                                username: {
-                                  type: 'string',
-                                },
-                                role: {
-                                  type: 'string',
-                                },
-                              },
-                            },
-                          },
+                        id: {
+                          type: 'number',
+                        },
+                        username: {
+                          type: 'string',
+                        },
+                        role: {
+                          type: 'string',
                         },
                       },
                     },
                   },
                 },
               },
-              400: {
-                description: 'User not authenticated',
-              },
-              401: {
-                description: 'User not authorized',
-              },
-              404: {
-                description: 'User not found',
-              },
-              500: {
-                description: 'An unexpected error occurred',
-              },
             },
           },
-        }
-      );
-
-    return app;
-  });
-
-  return app;
+        },
+      },
+      400: {
+        description: 'User not authenticated',
+      },
+      401: {
+        description: 'User not authorized',
+      },
+      404: {
+        description: 'User not found',
+      },
+      500: {
+        description: 'An unexpected error occurred',
+      },
+    },
+  } as OpenAPIV3.OperationObject,
 };
