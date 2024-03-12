92a4d3fb18c3991a31757015359d0198038f7276
Ji Park <ji.park@jpnws.com>
2024-03-11 21:36:01 -0700

    Add `createUser` method to the IUserRepository interface, and implement the method in the `UserRepository` class.

--- src/routes/users/UserRepository.ts
+++ src/routes/users/UserRepository.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 export interface IUserRepository {
   findById: (id: number) => Promise<any>;
+  createUser: (username: string, password: string) => Promise<any>;
 }
 
 export default class UserRepository implements IUserRepository {
@@ -12,9 +13,23 @@ export default class UserRepository implements IUserRepository {
   }
 
   public findById = async (id: number) => {
-    return await this.prisma.user.findUnique({
+    return this.prisma.user.findUnique({
       where: {
         id: id,
+      },
+      select: {
+        id: true,
+        username: true,
+        role: true,
+      },
+    });
+  };
+
+  public createUser = async (username: string, password: string) => {
+    return this.prisma.user.create({
+      data: {
+        username,
+        hash: await Bun.password.hash(password),
       },
       select: {
         id: true,
