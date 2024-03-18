4d3f5249e26bab9cd1324f4727f0cba6089802b1
Ji Park <ji.park@jpnws.com>
2024-03-18 14:52:31 -0700

    Increase rate limit to `200` just in case.

--- src/app.ts
+++ src/app.ts
@@ -57,7 +57,7 @@ export const createApp = (
 
   // Add rate limit to the app.
   if (rateLimit) {
-    app.use(rateLimit({ max: 100 }));
+    app.use(rateLimit({ max: 200 }));
   }
 
   // Define a health check route.
