d7ec00c4d09010dc5d95508989fc91310ea007cc
Ji Park <ji.park@jpnws.com>
2024-03-27 22:45:37 -0700

    Update CORS origin regex pattern in app.ts

--- src/app.ts
+++ src/app.ts
@@ -56,7 +56,7 @@ export const createApp = (
   if (cors) {
     app.use(
       cors({
-        origin: /discotime.netlify.app/,
+        origin: /^https?:\/\/discotime\.netlify\.app$/,
         methods: ['POST', 'GET', 'PUT', 'DELETE'],
       }),
     );
